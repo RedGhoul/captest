21e69de443d5d4c04d6bde20ba06cb3c14810476
RedGhoul <avaneesa.basappa@hey.com>
2022-05-18 15:32:26 -0400

    afsd

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Project;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::any('/projects', function () {
+    $project = Project::create([
+        'name' => 'London to Paris',
+        'tag_line' => 'cool',
+        'github_url' => 'cool2',
+        'live_url' => 'live_url',
+    ]);
+    $project->save();
+    return Project::all();
 });
